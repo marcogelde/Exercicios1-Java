7cd0403c11e60338adbd6e9380f60143b6aa5022
Marco Antonio Gelde <167691329+marcogelde@users.noreply.github.com>
2024-09-17 23:46:13 -0300

    Update and rename Ex5.java to Ex05.java

--- src/exercicios1Java/Ex05.java
+++ src/exercicios1Java/Ex05.java
@@ -1,6 +1,6 @@
 package exercicios1Java;
 
-public class Ex5 {
+public class Ex05 {
     public static void main(String[] args) {
         System.out.println("Precisamos usar o método main para iniciar o nosso aplicativo, ele é o método principal, sem ele o aplicativo não roda");
     }
