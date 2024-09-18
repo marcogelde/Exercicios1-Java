8d15f3b0882deec894a27f88678f78ad0e824251
Marco Antonio Gelde <marcogelde@hotmail.com>
2024-09-17 22:39:21 -0300

    Exercicio 1

---
+++ src/exercicios1Java/Ex1.java
@@ -0,0 +1,11 @@
+package exercicios1Java;
+
+//  Comentário de linha única
+/*
+    Comentário de múltiplas linhas
+*/
+public class Ex1 {
+    public static void main(String[] args){
+        System.out.println("Olá Mundo"); // Imprime, Olá Mundo!
+    }
+}
